8d708e8773fea5a68b383ced23d54ff0abd5e37b
Administrator <yangxuan8282@gmail.com>
2016-09-03 14:24:03 +0800

    update for new version ItChat

--- wechat_remote.py
+++ wechat_remote.py
@@ -4,13 +4,13 @@
 # remote for WeChat (based on ItChat: https://github.com/littlecodersh/ItChat)
 # modified from this demo(https://github.com/littlecodersh/ItChat/issues/24#issuecomment-228583833)
 # you can use this scripts to interact with your computer(such as RaspberryPi)
-# it should support most commands you use in terminal, for instance `uname` or `df -h` 
+# it should support most commands you use in terminal, for instance `uname` or `df -h`
 
 import os
 import sys
 #import subprocess
 import itchat
-import time 
+import time
 
 if os.name == 'posix' and sys.version_info[0] < 3:
 	import subprocess32 as subprocess
@@ -19,7 +19,7 @@ else:
 
 help = '''\
 WeChat remote:
-* download file: aria2c url
+* download file: diana add url
 * download video: youtube-dl url
 * online video: mpv url
 * & most commands you use in terminal
@@ -28,34 +28,34 @@ WeChat remote:
 
 @itchat.msg_register('Text')
 def remote(msg):
-	# comment next line if you can't send message to yourself
-	if not msg['FromUserName'] == msg['ToUserName']: return
+
+	if not msg['FromUserName'] == msg['ToUserName']: return  # comment this line if you can't send message to yourself
 	if msg['Text'] in ['help', u'帮助']:
 		return help
 	else:
 		commands = msg.get('Content', '')
 		args = commands.split()
-		
-		# Custom aliases 
+
+		# Custom aliases
 		if args[0] == 'bilidan':
 			args[0] = '/home/pi/BiliDan/bilidan.py'
 
 		if args[0] == 'dad':
 			args[0] = '/home/pi/diana/dad'
-		
+
 		if args[0] == 'diana':
 			args[0] = '/home/pi/diana/diana'
 
-		try: 
-			return subprocess.Popen(args, 
+		try:
+			proc =  subprocess.Popen(args,
 									# shell=True,
 									# executable='/bin/bash',
 									# stderr=subprocess.PIPE,
-									stdout=subprocess.PIPE).communicate()[0].strip()
+									stdout=subprocess.PIPE)
+			return proc.communicate()[0].strip()
 		except OSError as e:
 			return u'Commands/Files not found'
-	#except:
-		#return u'Error'		
 
-itchat.auto_login()
+
+itchat.auto_login(enableCmdQR = True, hotReload = True)
 itchat.run()
