1d84684ebfcb47334a0a8f7fefbc5d6af78debb0
Administrator <yangxuan8282@gmail.com>
2016-11-19 20:40:55 +0800

    update quick-install.sh and aria2 config file

--- quick-install.sh
+++ quick-install.sh
@@ -4,15 +4,17 @@ if ! [[ -f /usr/bin/wechat-remote ]]; then
   git clone https://github.com/yangxuan8282/wechat-remote /tmp/wechat-remote
   git clone https://github.com/baskerville/diana /tmp/diana
   sudo pip install itchat
+  sudo pip install subprocess32
   sudo cp /tmp/diana/diana /usr/bin
   sudo cp /tmp/diana/dad /usr/bin
   sudo chmod 755 /usr/bin/dad /usr/bin/diana
-  mkdir -p ~/.aria2/aria2
-  touch ~/.aria2/aria2.session
-  sudo cp /tmp/wechat-remote/aria2.conf ~/.config/.aira2/
-  sudo cp /tmp/wechat-remote/aria2c /etc/init.d/aria2c
-  sudo chmod +x /etc/init.d/aria2c
-  sudo service aria2c restart
+  mkdir -p $HOME/.config/aria2
+  cp aria2.conf $HOME/.config/aria2/aria2.conf
+  touch $HOME/.config/aria2/session.lock
+  sudo cp aria2.service /etc/systemd/user
+  systemctl --user start aria2
+  systemctl --user enable aria2  
+  dad start
   sudo cp /tmp/wechat-remote/wechat_remote.py /usr/bin/wechat-remote
   sudo chmod 755 /usr/bin/wechat-remote
 fi
